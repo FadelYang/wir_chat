5615e1b7c851d216448de8d1c1246b2dd6a0f005
fadelanumah <fadelanumah@gmail.com>
2024-11-07 17:05:24 +0700

    feat: Add Chatting Feature

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,43 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import { marked } from 'marked'
 
 export default function HomePage() {
-  const exampleMessage = "saya ingin membuat metaverse, siapa yang harus saya hubungi?"
+  const [inputMessage, setInputMessage] = useState('')
+  const [sessionId, setSessionId] = useState(() => sessionStorage.getItem('sessionId') || '')
+  const [conversations, setConversations] = useState(() => {
+    const savedConverstations = sessionStorage.getItem('conversations')
+    return savedConverstations ? JSON.parse(savedConverstations) : []
+  })
 
-  const exampleResponse = `Untuk membuat metaverse, Anda dapat mempertimbangkan beberapa pihak yang mungkin bisa membantu atau memiliki
-peran penting dalam pengembangan proyek metaverse tersebut. Berikut adalah beberapa contoh:\n\n1. **Perusahaan teknologi**: Perusahaan-perusahaan seperti Meta (sebelumnya Facebook), Google, Microsoft, dan Amazon yang sudah memiliki
-pengalaman dalam pengembangan teknologi virtual dan augmented reality (VR/AR) bisa menjadi pilihan.\n2. **Perusahaan blockchain**: Perusahaan-perusahaan yang fokus pada teknologi blockchain seperti Coinbase, Binance, atau ConsenSys bisa
-membantu dengan integrasi blockchain ke dalam proyek metaverse Anda.\n3. **Perusahaan gaming**: Perusahaan-perusahaan
-gaming seperti Unity Technologies (yang juga memiliki peran dalam pengembangan VR/AR) atau Epic Games bisa memberikan
-dukungan teknis dan sumber daya untuk pengembangan metaverse.\n4. **Institusi penelitian**: Institusi penelitian yang
-fokus pada teknologi metaverse, seperti University of California, Los Angeles (UCLA), atau Massachusetts Institute of
-Technology (MIT), bisa menyediakan sumber daya dan pengetahuan tentang teknologi terkait.\n5. **Konsultan teknologi**:
-Konsultan teknologi yang spesialisasi dalam pengembangan proyek metaverse seperti WIR Group bisa membantu Anda dengan
-strategi bisnis, desain sistem, dan implementasi teknologi.\n\nUntuk memulai, Anda mungkin ingin mempertimbangkan untuk
-menghubungi beberapa pihak ini untuk mendapatkan informasi lebih lanjut tentang potensi kerja sama atau dukungan yang
-dapat diberikan.`
-
-  const formattedResponse = marked(exampleResponse)
-
-  const [message, setMessage] = useState('')
+  // If sessionId or message change, update the conversations and sessionId value
+  useEffect(() => {
+    sessionStorage.setItem('conversations', JSON.stringify(conversations))
+    sessionStorage.setItem('sessionId', sessionId)
+  }, [conversations, sessionId]);
 
   function handleMessageInput(e) {
-    setMessage(e.target.value)
+    setInputMessage(e.target.value)
     e.target.style.height = '3rem'
     e.target.style.height = `${e.target.scrollHeight}px`
-    console.log(process.env.backend_url);
-    console.log(message);
   }
 
-  function getAnswer(e) {
+  const sendQuestionMessage = async (e) => {
     e.preventDefault()
 
+    const newInputMessage = { isUser: true, text: inputMessage }
+    setConversations((previousMessage) => [...previousMessage, newInputMessage])
+
     const data = {
-      message: message
+      message: inputMessage
     }
 
-    // fetch(process.env.BACK)
+    const response = await fetch(process.env.REACT_APP_BACKEND_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+
+    const answer = await response.json()
+
+    if (!sessionId && data.session_id) {
+      setSessionId(data.session_id)
+    }
+
+    const formattedBotMessage = marked(answer.outputs[0].outputs[0].results.message.data.text)
+    const botMessage = { isUser: false, text: formattedBotMessage }
+    setConversations((previousMessage) => [...previousMessage, botMessage])
   }
 
   return (
@@ -47,25 +58,32 @@ dapat diberikan.`
         {/* Navbar */}
         <div className="flex flex-col flex-grow">
           {/* Chat area */}
-          <div className={`${!exampleResponse ? 'hidden' : 'flex'} flex-col items-center chat-area flex-grow md:px-56 px-20 gap-10`}>
-              {/* Use dangerouslySetInnerHTML to inject the HTML */}
-              <div className='ml-auto bg-slate-100 py-2 px-4 rounded-2xl'>{exampleMessage}</div>
-              <div dangerouslySetInnerHTML={{ __html: formattedResponse }} />
-              <div className='ml-auto bg-slate-100 py-2 px-4 rounded-2xl'>{exampleMessage}</div>
-              <div dangerouslySetInnerHTML={{ __html: formattedResponse }} />
+          <div className={`${!conversations ? 'hidden' : 'flex'} flex-col chat-area flex-grow md:px-56 px-20 gap-10 mt-10`}>
+            {conversations.map((message, index) => (
+              <div
+                key={index}
+                className={message.isUser ? "ml-auto bg-slate-100 py-2 px-4 rounded-2xl" : "bot-message"}
+              >
+                {message.isUser ? (
+                  message.text
+                ) : (
+                  <div dangerouslySetInnerHTML={{ __html: message.text }} />
+                )}
+              </div>
+            ))}
           </div>
 
           {/* Input area */}
-          <div className={`sticky bottom-0 bg-white p-3 ${!exampleResponse ? 'my-auto' : ''}`}>
+          <div className={`sticky bottom-0 bg-white p-3 ${!conversations ? 'my-auto' : ''}`}>
             <div className="flex gap-1 justify-center">
               <textarea
-                value={message}
+                value={inputMessage}
                 onChange={handleMessageInput}
                 type="text"
                 placeholder="Kirim pertanyaan"
                 className="border border-black rounded-xl px-5 py-2 w-1/2 h-11 max-h-36 resize-none"
               />
-              <button className="rounded-xl text-white bg-black px-5 h-12 flex-shrink-1 items-start self-end">
+              <button onClick={sendQuestionMessage} className="rounded-xl text-white bg-black px-5 h-11 flex-shrink-1 items-start self-end">
                 Kirim
               </button>
             </div>

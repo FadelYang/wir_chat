80b3437a46004d38ad791f659491358572d74a0a
fadelanumah <fadelanumah@gmail.com>
2024-11-11 13:50:12 +0700

    feat: Add WIR Logo in Favicon and Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
 import profilePlaceholder from './../assets/profile-placeholder.jpeg'
+import wirLogo from '/wir.png'
 
 export default function () {
   return (
     <>
       <div className="sticky top-0 z-10 py-5 bg-white">
         <div className='container flex mx-auto'>
-          <div>WIR Chat</div>
+          <div className='flex items-center gap-2'>
+            <img src={wirLogo} className='w-10'/>
+            <div>WIR Chat</div>
+          </div>
           <div className='ms-auto'>
             {/* <img src={profilePlaceholder} className="w-10 rounded-full" /> */}
           </div>

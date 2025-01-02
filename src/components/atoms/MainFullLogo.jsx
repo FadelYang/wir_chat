4dc2585c81e0017ab045d2eb0af800a97376d369
fadelanumah <fadelanumah@gmail.com>
2025-01-02 10:48:04 +0700

    feat(dashboard): add basic dashboard template

---
+++ src/components/atoms/MainFullLogo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import wirFullLogo from '/logo-full.png';
+
+const MainFullLogo = ({ className }) => {
+  return <img src={wirFullLogo} className={className}></img>
+};
+
+export default MainFullLogo;

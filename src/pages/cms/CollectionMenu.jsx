1241ae6624d6e80cadd93e1eb2da34a0c9816834
fadelanumah <fadelanumah@gmail.com>
2025-01-02 17:19:14 +0700

    feat(dashboard): add database and collection menu pageheader content

--- src/pages/cms/CollectionMenu.jsx
+++ src/pages/cms/CollectionMenu.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import DashboardTemplate from '../../components/templates/cms/DashboardTemplate';
+import React from "react";
+import DashboardTemplate from "../../components/templates/cms/DashboardTemplate";
+import HeaderMenu from "../../components/organism/cms/HeaderMenu";
 
 const CollectionMenu = () => {
-  return (
-    <DashboardTemplate>CollectionMenu</DashboardTemplate>
-  )
-}
+  const breadcrumbPath = [
+    {name: "Dashboard", path: "/dashboard"},
+    {name: "Collections", path: "/dashboard/collections"},
+  ];
 
-export default CollectionMenu
+  return (
+    <DashboardTemplate>
+      <HeaderMenu name={`Collections`} breadcrumbPath={breadcrumbPath} />
+      <div className="mt-14">
+        <div className="flex justify-end">
+          <button className="bg-[#0F172A] text-white py-2 px-4 text-sm rounded">
+            Add new collections
+          </button>
+        </div>
+      </div>
+    </DashboardTemplate>
+  );
+};
+
+export default CollectionMenu;
--- src/pages/cms/DatabaseMenu.jsx
+++ src/pages/cms/DatabaseMenu.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import DashboardTemplate from '../../components/templates/cms/DashboardTemplate';
+import DashboardTemplate from "../../components/templates/cms/DashboardTemplate";
+import HeaderMenu from "../../components/organism/cms/HeaderMenu";
 
 const DatabaseMenu = () => {
+  const breadcrumbPath = [
+    {name: "Dashboard", path: "/dashboard"},
+    {name: "Databases", path: "/dashboard/databases"},
+  ];
+
   return (
-    <DashboardTemplate>DatabaseMenu</DashboardTemplate>
+    <DashboardTemplate>
+      <HeaderMenu name={`Databases`} breadcrumbPath={breadcrumbPath} />
+      <div className="mt-14">
+        <div className="flex justify-end">
+          <button className="bg-[#0F172A] text-white py-2 px-4 text-sm rounded">
+            Add new database
+          </button>
+        </div>
+      </div>
+    </DashboardTemplate>
   );
 };
 

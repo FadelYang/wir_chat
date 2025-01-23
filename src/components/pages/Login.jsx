77a980bd8be9149075e35581662790a864d5a503
fadelanumah <fadelanumah@gmail.com>
2025-01-23 15:18:14 +0700

    feat: add login feature

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,31 +1,92 @@
 import React, { useState } from "react";
+import { auth } from "../../firebase/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const onLogin = async (e) => {
+    e.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate("/dashboard");
+        setError(false);
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        setError(true);
+        console.log({ error });
+        if (errorMessage == "Firebase: Error (auth/too-many-requests).") {
+          setErrorMessage(
+            "Terlalu banyak percobaan, silahkan coba lagi beberapa saat"
+          );
+        } else {
+          setErrorMessage("Email atau password salah");
+        }
+        console.log(errorCode, errorMessage);
+      });
+  };
 
   return (
     <>
       <div className="flex h-screen justify-center items-center">
-        <div className="flex flex-col gap-5 border rounded py-5 px-10 shadow">
+        <form
+          className="flex flex-col gap-5 border rounded py-5 px-10 shadow"
+          onSubmit={onLogin}
+        >
           <div className="text-center text-xl font-medium">Login Form</div>
-          <div className="flex flex-col gap-1">
+          <div className="flex flex-col gap-2">
             <label
               className="block text-sm font-bold text-gray-700"
-              htmlFor="language"
+              htmlFor="email"
             >
-              username
+              Email
             </label>
             <input
               className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-              id="collectionFile"
-              name="language"
-              type="text"
-              // disabled
-              // value={selectedLanguage}
+              id="email"
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
             />
+            <div className="flex flex-col gap-1">
+              <label
+                className="block text-sm font-bold text-gray-700"
+                htmlFor="password"
+              >
+                Passowrd
+              </label>
+              <input
+                className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                id="passwrd"
+                name="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              {error && (
+                <div className="text-wrap text-sm font-medium text-red-500 max-w-[274px]">
+                  {errorMessage}
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+          <div className="flex justify-end">
+            <button type="submit">Login</button>
+          </div>
+        </form>
       </div>
     </>
   );

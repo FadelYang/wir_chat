b817f713672551abe4cb206042d1fe193b50ea15
fadelanumah <fadelanumah@gmail.com>
2025-02-12 16:16:38 +0700

    fix: fix vite config for sub directory deployment

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import { config } from 'dotenv'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { config } from "dotenv";
+import react from "@vitejs/plugin-react";
 
-config()
+config();
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   define: {
-    'process.env': process.env
-  }
-})
+    "process.env": process.env,
+  },
+});

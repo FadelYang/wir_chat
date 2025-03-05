c4a28b8e3eb22caa04655565dff13bc63dba1c9b
fadelanumah <fadelanumah@gmail.com>
2025-03-05 15:16:18 +0700

    fix(profile): add user profile page

--- src/components/pages/cms/Dashboard.jsx
+++ src/components/pages/cms/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import DashboardTemplate from "../../templates/cms/DashboardTemplate";
 import { useAuth } from "../../../context/AuthContext";
+import HeaderMenu from '../../organism/cms/HeaderMenu';
 
 const Dashboard = () => {
   const { user, setUser, loading } = useAuth();
+  const breadcrumbPath = [
+    {name: "Dashboard", path: "/dashboard"},
+  ];
 
   return (
     <DashboardTemplate>
-      <div>
+      <HeaderMenu name={`Dashboard`} breadcrumbPath={breadcrumbPath} />
+      <div className='mt-14'>
         <h1 className="text-xl">Halo, {loading ? "..." : user.email}</h1>
       </div>
     </DashboardTemplate>
--- src/components/pages/cms/UserProfile.jsx
+++ src/components/pages/cms/UserProfile.jsx
@@ -1,7 +1,20 @@
-const UserProfile = () => {
-  return (
-    <div>UserProfile</div>
-  )
-}
+import { useAuth } from "../../../context/AuthContext";
+import HeaderMenu from "../../organism/cms/HeaderMenu";
+import DashboardTemplate from "../../templates/cms/DashboardTemplate";
 
-export default UserProfile
+const UserProfile = () => {
+  const { user } = useAuth();
+  const breadcrumbPath = [
+    { name: "Dashboard", path: "/dashboard" },
+    { name: user.email, path: `/dashboard/users/${user.uid}` },
+  ];
+
+  return (
+    <DashboardTemplate>
+      <HeaderMenu name={`User Profile`} breadcrumbPath={breadcrumbPath} />
+      <div className="mt-14">{user.email}</div>
+    </DashboardTemplate>
+  );
+};
+
+export default UserProfile;

d635ad86d906d18db7d7f31872ca69d83865ca8f
fadelanumah <fadelanumah@gmail.com>
2025-03-24 12:21:13 +0700

    feat(user): add update functionality

--- src/components/organism/cms/datatables/UserTable.jsx
+++ src/components/organism/cms/datatables/UserTable.jsx
@@ -1,15 +1,60 @@
-import { useEffect } from "react";
-import { getUsers } from "../../../../firebase/userService";
+import { useEffect, useState } from "react";
+import { changeActiveStatus, getUsers } from "../../../../firebase/userService";
 import BaseTable from "./BaseTable";
 import { useUsers } from "../../../../context/UserContext";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 const UserTable = () => {
   const { users, setUsers } = useUsers();
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedRow, setSelectedRow] = useState("");
 
   const fetchUsers = async () => {
     const data = await getUsers();
     setUsers(data);
+  };
+
+  const changeUserActiveStatus = async (userEmail, userId, isActiveStatus) => {
+    const isConfirm = confirm("Are you sure?");
+    if (!isConfirm) return;
+    setIsLoading(true);
+    setSelectedRow(userId);
+
+    try {
+      const updateUrl = `${
+        process.env.REACT_APP_BACKEND_URL
+      }/update-user/${userEmail}/${!isActiveStatus}`;
+      const response = await fetch(updateUrl, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        console.log("Failed");
+        throw new Error("Failed to update data");
+      }
+
+      await changeActiveStatus(userId, isActiveStatus);
+
+      alert(
+        `Success: User status changed to ${
+          !isActiveStatus ? "Active" : "Non-Active"
+        }`
+      );
+      setIsLoading(false);
+      setSelectedRow("false");
+    } catch (error) {
+      console.error("Error updating user:", error.message);
+
+      await changeActiveStatus(userId, !isActiveStatus);
+
+      alert("Failed to change user active status. Changes reverted.");
+
+      setIsLoading(false);
+      setSelectedRow("false");
+    }
   };
 
   useEffect(() => {
@@ -34,15 +79,25 @@ const UserTable = () => {
       header: "Is Acttive",
       cell: ({ row }) => (
         <>
-          <div className={`w-24 ${!row.original.is_status ? "bg-green-500 rounded-xl" : "bg-red-500"}`}>
-            <p className='text-center text-sm text-white'>{!row.original.is_status ? "Active" : "Non Active"}</p>
-          </div>
+          {isLoading && selectedRow === row.original.id ? (
+            <div className="w-24 bg-gray-300 rounded-xl animate-pulse h-6"></div>
+          ) : (
+            <div
+              className={`w-24 ${
+                row.original.is_active ? "bg-green-500" : "bg-red-500"
+              } rounded-xl`}
+            >
+              <p className="text-center text-sm text-white">
+                {row.original.is_active ? "Active" : "Non Active"}
+              </p>
+            </div>
+          )}
         </>
       ),
     },
     {
       header: "Action",
-      cell: (
+      cell: ({ row }) => (
         <Menu>
           <MenuButton className={"bg-black text-white py-2 px-4 rounded"}>
             Menu
@@ -57,9 +112,18 @@ const UserTable = () => {
               </a>
             </MenuItem>
             <MenuItem>
-              <a className="block data-[focus]:bg-blue-100" href="/support">
+              <button
+                className="block data-[focus]:bg-blue-100"
+                onClick={() =>
+                  changeUserActiveStatus(
+                    row.original.email,
+                    row.original.id,
+                    row.original.is_active
+                  )
+                }
+              >
                 Disable
-              </a>
+              </button>
             </MenuItem>
           </MenuItems>
         </Menu>
--- src/firebase/userService.js
+++ src/firebase/userService.js
@@ -1,4 +1,11 @@
-import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { auth, db } from "./firebase";
 import {
   EmailAuthProvider,
@@ -25,43 +32,64 @@ export const getUsers = async () => {
   }
 };
 
+export const changeActiveStatus = async (docuemntId, isActiveStatus, customMessage) => {
+  try {
+    const changeStatus = !isActiveStatus;
+
+    const userRef = doc(db, "users", docuemntId);
+    await updateDoc(userRef, {
+      is_active: changeStatus,
+    });
+  } catch (error) {
+    console.error("Error change user active status", error.message);
+    throw new Error("Failed to change user acttive status" + error.message);
+  }
+};
+
 export const getUserByEmail = async (email) => {
   const users = await getUsers();
-  const userWithEmail = users.map(item => item.email);
+  const userWithEmail = users.map((item) => item.email);
   const isUserExists = userWithEmail.includes(email);
-  return isUserExists
-}
+  return isUserExists;
+};
 
 export const getLoggedUserRole = async () => {
   const loggedUser = auth.currentUser;
   const loggedUserUid = loggedUser.uid;
   const loggedUserData = (await getDoc(doc(db, "users", loggedUserUid))).data();
 
-  return loggedUserData.role
-}
+  return loggedUserData.role;
+};
 
 export const registerUser = async (email, password, role, adminPassword) => {
   try {
     const currentAdmin = auth.currentUser;
     if (!currentAdmin) throw new Error("Admin not logged in");
 
-    const credential = EmailAuthProvider.credential(currentAdmin.email, adminPassword);
+    const credential = EmailAuthProvider.credential(
+      currentAdmin.email,
+      adminPassword
+    );
     await reauthenticateWithCredential(currentAdmin, credential);
 
-    const createdUser = await createUserWithEmailAndPassword(auth, email, password);
-    const uid = createdUser.user.uid
+    const createdUser = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const uid = createdUser.user.uid;
 
     await setDoc(doc(db, "users", createdUser.user.uid), {
       email,
       role,
       createdAt: new Date(),
-      is_active: true
+      is_active: true,
     });
 
     await signOut(auth);
 
     await signInWithEmailAndPassword(auth, currentAdmin.email, adminPassword);
-    window.location.href = "/dashboard/users"
+    window.location.href = "/dashboard/users";
   } catch (error) {
     console.error("Error registering user:", error.message);
     throw new Error(error.message || "User creation failed.");
